aa892378447f13aa2ae7298abc615f862329074d
jenlij <jljohns1216@gmail.com>
2017-08-09 14:54:37 -0400

    removes node modules

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const pg = require('pg-promise')();
+
+const dbConfig = {
+    host: 'localhost',
+    port: 5432,
+    database: 'exercises',
+    user: 'jenlij',
+    //password: 'user-password'
+};
+const db = pg(dbConfig);
+
+function allFacilities() {
+    return db.query()
+}
